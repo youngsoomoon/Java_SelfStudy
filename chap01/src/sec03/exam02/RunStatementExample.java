9963dafe7cd66a0ba938bdf2189f6c53267a6559
DESKTOP-FOFAO14\MOON <moonyoungsu@naver.com>
2021-09-16 22:39:11 +0900

    add JavaBasic
    자바 기초내용 추가

---
+++ chap01/src/sec03/exam02/RunStatementExample.java
@@ -0,0 +1,16 @@
+package sec03.exam02;
+
+public class RunStatementExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		
+		System.out.println(result);
+		
+	}
+
+}
