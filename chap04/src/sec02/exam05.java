b1590164791eb4a0cd27f2bf2d6adc73784a9774
DESKTOP-FOFAO14\MOON <moonyoungsu@naver.com>
2021-09-19 17:04:08 +0900

    add ForMultiplicationTableExample
    중첩 for문을 이용한 구구단 예제추가

---
+++ chap04/src/sec02/exam05.java
@@ -0,0 +1,15 @@
+package sec02;
+
+public class exam05 {
+
+	public static void main(String[] args) {
+		for (int m=2; m<=9; m++) {
+			System.out.println("***" + m + "단 입니다.***");
+			for (int n=1; n<=9; n++) {
+				System.out.println( m + " x " + n + " = " + (m*n) );
+			}
+		}
+
+	}
+
+}
