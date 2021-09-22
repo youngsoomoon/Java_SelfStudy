f0ed74c753ae702c9d09f5c5970e531fc4cb7f95
DESKTOP-FOFAO14\MOON <moonyoungsu@naver.com>
2021-09-22 20:46:25 +0900

    add StringEqualsExample
    참조 , 문자열비교 예제

---
+++ chap05/src/sec01/exam01.java
@@ -0,0 +1,34 @@
+package sec01;
+
+public class exam01 {
+
+	public static void main(String[] args) {
+		String strVar1 = "문영수";
+		String strVar2 = "문영수";
+		
+		if(strVar1 == strVar2) {
+			System.out.println("strVar1과 strVar2는 참조가 같음");
+		} else {
+			System.out.println("strVar1과 strVar2는 참조가 다름");
+		}
+		
+		if(strVar1.equals(strVar2)) {
+			System.out.println("strVar1과 strVar2는 문자열이  같음");
+		}
+		
+		String strVar3 = new String("문영수");
+		String strVar4 = new String("문영수");
+		
+		if(strVar3 == strVar4) {
+			System.out.println("strVar3과 strVar4는 참조가 같음");
+		} else {
+			System.out.println("strVar3과 strVar4는 참조가 다름");
+		}
+		
+		if(strVar3.equals(strVar4)) {
+			System.out.println("strVar3과 strVar4는 문자열이  같음");
+		}		
+
+	}
+
+}
