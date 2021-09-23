43a8f52dffad83fcfa1aaf523f216e2ebd043e96
DESKTOP-FOFAO14\MOON <moonyoungsu@naver.com>
2021-09-23 20:12:29 +0900

    add sum_avg
    확인문제5번 _ 2차원배열에서 총합,평균값 구하기

---
+++ chap05/src/verify/Exam05.java
@@ -0,0 +1,22 @@
+package verify;
+
+public class Exam05 {
+	public static void main(String[] args) {
+		int[][] array = { { 95, 86 }, { 83, 92, 96 }, { 78, 83, 93, 87, 88 } };
+
+		int sum = 0;
+		double avg = 0.0;
+		int num = 0;
+		
+		for (int i = 0; i<array.length;i++) {
+			for (int j = 0; j<array[i].length;j++) {
+				sum += array[i][j];
+				num += 1;
+			}
+		}
+		avg = (double) sum/num;
+
+		System.out.println("sum: " + sum);
+		System.out.println("avg: " + avg);
+	}
+}
