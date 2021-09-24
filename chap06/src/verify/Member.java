88e894f0e88c1074c7a2c47396190885740941ba
DESKTOP-FOFAO14\MOON <moonyoungsu@naver.com>
2021-09-24 20:13:01 +0900

    add Class_field
    클래스_필드 선언예제

---
+++ chap06/src/verify/Member.java
@@ -0,0 +1,9 @@
+package verify;
+
+public class Member {
+	//ÇÊµå
+	String name;
+	String id;
+	String password;
+	int age;
+	}
---
+++ chap06/src/verify/MemberExample.java
@@ -0,0 +1,15 @@
+package verify;
+
+public class MemberExample {
+
+	public static void main(String[] args) {
+		Member member = new Member();
+		
+		member.name = "¹®¿µ¼ö";
+		member.age = 23;
+		System.out.println(member.name);
+		System.out.println(member.age);
+
+	}
+
+}
