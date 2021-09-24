43d010a536e5b7565c1143117649b0e406078078
DESKTOP-FOFAO14\MOON <moonyoungsu@naver.com>
2021-09-24 20:16:56 +0900

    add this.field
    클래스_this.를이용한 생성자수정

---
+++ chap06/src/verify/Member2.java
@@ -0,0 +1,13 @@
+package verify;
+
+public class Member2 {
+	//Field
+	String name;
+	String id;
+	
+	//Constructor
+	public Member2(String name, String id) {
+		this.name = name;
+		this.id = id;
+	}
+}
