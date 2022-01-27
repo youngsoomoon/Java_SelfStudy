1aed90f669c64f73a19cd885f4ec730f6c17fc45
Moonyoungsoo <moonyoungsu@naver.com>
2022-01-27 22:15:03 +0900

    fixed untracked files

---
+++ Boj/src/basic/Remainer_3052.java
@@ -0,0 +1,22 @@
+package basic;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Remainer_3052 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] input = new int[10];
+		for (int i = 0; i < 10; i++) {
+			input[i] = sc.nextInt();
+		}
+		HashSet<Integer> cnt = new HashSet<Integer>();
+		
+		for (int i = 0; i < 10; i++) {
+			cnt.add(input[i]%42);
+		}
+		System.out.println(cnt.size());
+	}
+
+}
