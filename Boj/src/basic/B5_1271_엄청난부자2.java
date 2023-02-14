0cbb80ff7e2a5d40f0cda0df363cd77361a1789d
ysmoon22 <ysmoon22@koreaap.com>
2023-02-14 17:28:27 +0900

    add BOJ_B5_1271
    엄청난부자2 문제 풀이

--- Boj/src/basic/B5_1271_엄청난부자2.java
+++ Boj/src/basic/B5_1271_엄청난부자2.java
@@ -1,24 +1,20 @@
 package basic;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.math.BigInteger;
+import java.util.Scanner;
 
 public class B5_1271_엄청난부자2 {
-public static void main(String[] args) throws IOException {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st = new StringTokenizer(br.readLine());
-	
-	int N = Integer.parseInt(st.nextToken());
-	int M = Integer.parseInt(st.nextToken());
-	int result;
-	
-	result = N / M;
-	int dummy = N % M;
-	
-	System.out.println("result: "+result);
-	System.out.println("dummy: "+dummy);
-}
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		BigInteger N = sc.nextBigInteger();
+		BigInteger M = sc.nextBigInteger();
+
+		BigInteger result = N.divide(M);
+		BigInteger dummy = N.remainder(M);
+
+		System.out.println(result);
+		System.out.println(dummy);
+	}
 }
